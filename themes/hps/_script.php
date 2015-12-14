829add0b3acbc7beb0e09f637c38396ae335f712
unisexx <unisexx@gmail.com>
2015-12-14 15:17:23 +0700

    .....

--- themes/hps/_script.php
+++ themes/hps/_script.php
@@ -2,7 +2,6 @@
 <script type="text/javascript" src="media/js/jquery-1.8.2.min.js"></script>
 <script type="text/javascript" src="media/js/bootstrap/js/bootstrap.js"></script>
 <script type="text/javascript" src="media/js/jquery.validate.min.js"></script>
-<script type="text/javascript" src="media/js/jquery.validate.min.js"></script>
 <?php echo js_notify(); ?>
 <?php echo js_lightbox(); ?>
 <script type="text/javascript" src="themes/hps/myscript.js"></script>
