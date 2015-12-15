4630157808096940e2a63c5853f8fe6396e98ca8
unisexx <unisexx@hotmail.com>
2015-12-15 16:18:00 +0700

    ....

---
+++ application/modules/reports/controllers/reports.php
@@ -0,0 +1,14 @@
+<?php
+class Reports extends Public_Controller {
+
+	function __construct()
+	{
+		parent::__construct();	
+	}
+	
+	//สรุปผลการดำเนินงานโครงการศูนย์เด็กเล็กปลอดโรคโดยรวมทั้งหมด
+	function nursery_regis(){
+		
+	}
+}
+?>
